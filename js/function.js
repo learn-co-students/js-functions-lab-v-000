78ac0fe3f52cbe916b878d310b80fa2c77b216ac
Karuna <imkaruna@gmail.com>
2016-02-07 23:53:36 +0000

    Done.

---
+++ js/function.js
@@ -0,0 +1,15 @@
+function happyHolidays() {
+  return "Happy Holidays!!";
+}
+
+function happyHolidaysWithName(name) {
+  return "Happy Holidays, " + name;
+}
+
+function HappyAnyHoliday (holiday, name) {
+  return "Happy "+ holiday +", "+name;
+}
+
+function holidayCountDown (holiday, days) {
+  return "It\'s "+ days + "days until "+holiday+"!";
+}
