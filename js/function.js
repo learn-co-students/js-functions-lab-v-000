ede0c56c5a75fc5b590ae8742d332fe0f4479376
John Anthony Rivera <askinganthony@gmail.com>
2016-02-11 19:48:26 -0800

    Done.

---
+++ js/function.js
@@ -0,0 +1,4 @@
+function happyHolidays(){ return "Happy Holidays!!"; }
+function happyHolidaysWithName(name){ return "Happy Holidays, "+name; }
+function happyAnyHoliday(holiday, name){ return "Happy "+holiday+", "+name; }
+function holidayCountDown(days, holiday){ return "It's "days+" days till "+holiday}
