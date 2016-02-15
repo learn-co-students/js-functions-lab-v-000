67b9c1f4f4146a56c632b49bd5d9790128c2a632
Mitul Mistry <mitul.mistry1@gmail.com>
2016-02-15 22:55:51 +0000

    Done.

---
+++ js/function.js
@@ -0,0 +1,15 @@
+function happyHolidays() {
+  return "Happy Holidays!!";
+}
+
+function happyHolidaysWithName(name) {
+  return "Happy Holidays, " + name;
+}
+
+function happyAnyHoliday(personName, holiday) {
+  return "Happy " + holiday + ", " + personName;
+}
+
+function holidayCountDown(holiday, numDays) {
+  return "It's " + numDays + " days till " + holiday + "!";
+}
