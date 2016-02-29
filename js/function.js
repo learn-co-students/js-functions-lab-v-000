a9a3f342ebbdfee31b815e7f1b54cfbf607957d5
Chen Yang <yangc5@mail.uc.edu>
2016-02-29 21:49:08 +0000

    Done.

---
+++ js/function.js
@@ -0,0 +1,15 @@
+function happyHolidays(){
+  return "Happy Holidays!!";
+}
+
+function happyHolidayWithName(person){
+  return "Happy Holiday "+person+". "
+}
+
+function happyAnyholiday(holiday, person){
+  return "Happy "+holiday+" "+person+". "
+}
+
+function holidayCountDown(days, holiday){
+  return "It's "+days+" days till "+holiday+"!";
+}
