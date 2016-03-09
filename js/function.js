810b8f30664d327acbe4b968a8535b19ad071253
Edgar Gonzalez <edgar.rawr@gmail.com>
2016-03-09 01:28:16 +0000

    Done.

---
+++ js/function.js
@@ -0,0 +1,15 @@
+var happyHolidays = function() {
+  return("Happy Holidays!!")
+} 
+
+var happyHolidaysWithName = function(name) {
+  return("Happy Holidats, " + name)  
+}
+
+var happyAnyHoliday = function(name,holiday) {
+  return("Happy " + holiday + ", " + name )
+}
+
+var holidayCountDown = function(holiday,days_left) {
+  return("It's " + days_left + " days till " + holiday )
+}
