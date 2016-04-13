975215a3a7afdc8997ce5fbafc620828e27d546a
pickledyamsman <pickledyamsman@gmail.com>
2016-04-13 12:19:57 -0400

    Done

---
+++ js/function.js
@@ -0,0 +1,15 @@
+function happyHolidays(){
+  return "Happy Holidays!!";
+}
+
+function happyHolidaysWithName(name){
+  return "Happy Holidays, " + name;
+}
+
+function happyAnyHoliday(holiday, name){
+  return "Happy " + holiday + ", " + name;
+}
+
+function holidayCountDown(holiday, numOfDays){
+  return "It's " + numOfDays + " days till " + holiday + "!";
+}
