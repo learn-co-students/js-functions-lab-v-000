dcc98c27691d181c9d93d14f2a3c6bbf4e671824
Kinsey <kinseybasinger@gmail.com>
2016-04-29 01:50:57 +0000

    Done.

---
+++ js/function.js
@@ -0,0 +1,15 @@
+function happyHolidays() {
+  return = "Happy Holidays!";
+}
+
+function happyHolidaysWithName() {
+  return = "Happy Holidays," + name;
+}
+
+function happyAnyHoliday() {
+  return = "Happy" + holiday "," + name;
+}
+
+function holidayCountDown() {
+  return "It's " + days + " days till " + holiday + "!";
+}
