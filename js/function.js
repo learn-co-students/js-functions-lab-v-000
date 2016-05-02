1798cc7414f251e3e24f0d59c52f0941b5fecf60
rocketNeck <homestarflyer@gmail.com>
2016-05-02 15:19:13 -0400

    Done.

---
+++ js/function.js
@@ -0,0 +1,14 @@
+function happyHolidays(){
+  return "Happy Holidays!!";
+}
+
+function happyHolidaysWithName(name){
+  return "Happy Holidays, " + name;
+}
+function happyAnyHoliday(name, holiday) {
+  return "Happy " holiday + ", " + name;
+}
+
+function holidayCountDown(holidayName, daysTillHoliday) {
+  return "It's " + daysTillHoliday + " days till " holidayName + "!"
+}
