2f48934356c4f0dc7b036441c6309948ea473324
Brian Holland <brian.holland@proatria.com>
2016-05-03 12:36:38 -0500

    Done.

---
+++ js/function.js
@@ -0,0 +1,15 @@
+function happyHolidays () {
+  return "Happy Holidays!!";
+};
+
+function happyHolidaysWithName (name) {
+  return "Happy Holidays, " + name;
+};
+
+function happyAnyHoliday (holiday_name, name) {
+  return "Happy " + holiday_name + ", " + name;
+};
+
+function holidayCountDown (holiday_name, days_until) {
+  return "It's " + days_until.toString() + " till " + holiday_name;
+};
