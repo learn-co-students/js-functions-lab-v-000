c0f7feaf5c1fa8c54b5e3a9ed2a3d0f938acfcf3
Morgan VanYperen <morgvanny@gmail.com>
2016-06-14 20:58:22 +0000

    Done.

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function daysUntilHoliday(days, holiday) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
