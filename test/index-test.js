940ec9d1bc3bfd0c372234ab2fecf5d588c195c9
reichardn <reichardn@gmail.com>
2016-07-23 16:44:28 -0400

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -16,21 +16,21 @@ describe('functions', () => {
     })
   })
 
-  // describe('happyHolidaysTo(name)', () => {
-  //   it('returns "Happy holidays, ${name}!"', () => {
-  //     expect(happyHolidaysTo('you')).to.equal('Happy holidays, you!')
-  //   })
-  // })
+  describe('happyHolidaysTo(name)', () => {
+    it('returns "Happy holidays, ${name}!"', () => {
+      expect(happyHolidaysTo('you')).to.equal('Happy holidays, you!')
+    })
+  })
 
-  // describe('happyHolidayTo(holiday, name)', () => {
-  //   it('returns "Happy ${holiday}, ${name}!"', () => {
-  //     expect(happyHolidayTo('Independence Day', 'you')).to.equal('Happy Independence Day, you!')
-  //   })
-  // })
+  describe('happyHolidayTo(holiday, name)', () => {
+    it('returns "Happy ${holiday}, ${name}!"', () => {
+      expect(happyHolidayTo('Independence Day', 'you')).to.equal('Happy Independence Day, you!')
+    })
+  })
 
-  // describe('holidayCountdown(holiday, days)', () => {
-  //   it('returns "It\'s ${days} until ${holiday}!"', () => {
-  //     expect(holidayCountdown("Mother's Day", 20)).to.equal("It's 20 days until Mother's Day!")
-  //   })
-  // })
+  describe('holidayCountdown(holiday, days)', () => {
+    it('returns "It\'s ${days} until ${holiday}!"', () => {
+      expect(holidayCountdown("Mother's Day", 20)).to.equal("It's 20 days until Mother's Day!")
+    })
+  })
 })
