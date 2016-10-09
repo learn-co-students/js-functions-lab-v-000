bc8a5c58b03de298eb4cd12f0d88f78d908c158c
Tanner Lancaster <saltillokid11@yahoo.com>
2016-10-09 17:33:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return "Happy holidays, " + name + "!";
+}
+
+function happyHolidayTo(holiday, name) {
+  return "Happy " + holiday + ", " + name + "!";
+}
+
+function holidayCountdown(holiday, days) {
+  if (typeof holiday === "undefined"){
+    holiday = "random";
+  }
+  return "It\'s " + days + " days until " + holiday + "!";
+}
