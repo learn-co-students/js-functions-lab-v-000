d4d457b18b1f647975830e15e437d3d6f2b197e7
Branden Miller <milleb94@gmail.com>
2016-10-25 05:35:13 +0000

    Done.

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays ${name} !`;
+  return `Happy holidays, ${name}!`;
 }
 
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
 
-function daysUntilHoliday(days, holiday) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`;
 }
