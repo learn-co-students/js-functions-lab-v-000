ccefcb2ec66a6c1b2b15e349c8106f83511728d1
bmolina-nyc <bruce.d.molina@gmail.com>
2017-01-09 12:09:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // write your code below!
+
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+
+function happyHolidaysTo(name) {
+  if (typeof name === "undefined") {
+    name = "you";
+  }
+  return "Happy holidays, " +  name + "!"
+}
+
+function happyHolidayTo(holiday, name) {
+  if (typeof holiday === "undefined") {
+    holiday = "holiday"
+  }
+
+  return "Happy " + holiday + ", " + name + "!" 
+}
+
+
+function holidayCountdown(holiday, days) {
+  return "It\'s " + days + " days until " + holiday + "!"
+}
+
