eb0bc37400de093975986e7994bd4d6aa02201e9
Daniela Carvajal <d.carvajal.agama@gmail.com>
2017-05-21 02:02:38 +0000

    done right this time

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 // write your code below!
 //var name = "Dani";
 
-function happyHolidays(string){
+//function happyHolidays(string){
+  //return "Happy holidays!"
+//};
+
+//function happyHolidaysTo(name){
+  //return "Happy holidays, you!"
+//};
+
+//function happyHolidayTo(holiday, name){
+  //return "Happy Independence Day, you!"
+//};
+
+//function holidayCountdown(holiday, days){
+  //return "It's 20 days until Mother's Day!"
+//};
+function happyHolidays() {
   return "Happy holidays!"
-};
+}
 
-function happyHolidaysTo(name){
-  return "Happy holidays, you!"
-};
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!` // Now I know this `` is the correct way to interpolate in JS
+}
 
-function happyHolidayTo(holiday, name){
-  return "Happy Independence Day, you!"
-};
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
 
-function holidayCountdown(holiday, days){
-  return "It's 20 days until Mother's Day!"
-};
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
