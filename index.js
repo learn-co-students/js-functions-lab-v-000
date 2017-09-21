98eb93bc9285dd65951c89391f09f5a2b2312f00
smithwebtek <brad@smithwebtek.com>
2017-09-21 15:30:18 -0400

    buildt another days-t-xmas counter based on today's date

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+ 
 function happyHolidays(){
   return "Happy holidays!";
 }
@@ -16,3 +17,14 @@ function happyHolidayTo(holiday, name){
 function holidayCountdown(name, days){
   return "It's " + days + " days until " + name + "!";
 }
+
+function daysToXmas(){
+  var today = new Date();
+  var xmas = new Date('December 25, 2017'); // Set date of xmas
+  var msPerDay = 24 * 60 * 60 * 1000; // Number of milliseconds per day
+  var daysLeft = (xmas.getTime() - today.getTime());
+  var daysLeft = Math.round((daysLeft) / msPerDay); 
+
+  console.log("There are " + daysLeft + " days left until xmas.");
+}
+
